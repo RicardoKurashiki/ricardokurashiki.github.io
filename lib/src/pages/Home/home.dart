45ff3bba0dc98ef6036d464a0407ce81288b82d1
RicardoKurashiki <RicardoKurashiki@users.noreply.github.com>
2021-01-22 22:00:54 -0300

    Revert "Revert "Fixing bugs""
    This reverts commit 7e38559d9b6bbd699710398d95cd8186abfaabae.

--- lib/src/pages/Home/home.dart
+++ lib/src/pages/Home/home.dart
@@ -5,91 +5,98 @@ import '../../components/components.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: wheatColor,
-      body: Row(
-        children: [
-          CustomDrawer(
-            image: myFace,
-            name: 'Ricardo Godoi Kurashiki',
-          ),
-          Expanded(
-              child: Stack(
-            children: [
-              //Introducao
-              Positioned(
-                top: 20,
-                left: 20,
-                bottom: 350,
-                right: 20,
-                child: CustomContainer(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Olá, meu nome é Ricardo Godoi Kurashiki, tudo bem?",
-                        style: TextStyle(
-                          color: wheatColor,
-                          fontSize: 40,
+    return LayoutBuilder(builder: (context, c) {
+      final h = c.constrainHeight() / 16;
+      final w = c.constrainWidth() / 9;
+      return Scaffold(
+        backgroundColor: wheatColor,
+        body: Row(
+          children: [
+            CustomDrawer(
+              image: myFace,
+              name: 'Ricardo Godoi Kurashiki',
+              height: h,
+              width: w,
+            ),
+            Expanded(
+                child: Stack(
+              children: [
+                //Introducao
+                Positioned(
+                  top: 0.15 * h,
+                  left: 0.15 * w,
+                  bottom: 9 * h,
+                  right: 0.15 * w,
+                  child: CustomContainer(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Olá, meu nome é Ricardo Godoi Kurashiki, tudo bem?",
+                          style: TextStyle(
+                            color: wheatColor,
+                            fontSize: 0.85 * h,
+                          ),
                         ),
-                      ),
-                      Divider(
-                        color: Colors.transparent,
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Text(
-                            textIntro,
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: redColor,
-                              fontSize: 40,
+                        Divider(
+                          color: Colors.transparent,
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              textIntro,
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: redColor,
+                                fontSize: 0.85 * h,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              //Habilidades
-              Positioned(
-                top: 325,
-                left: 20,
-                bottom: 20,
-                right: 20,
-                child: CustomContainer(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Minhas habilidades',
-                        style: TextStyle(
-                          color: redColor,
-                          fontSize: 40,
-                        ),
-                      ),
-                      Divider(
-                        color: redColor,
-                        thickness: 2,
-                      ),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              SkillBoxes(),
-                            ],
+                //Habilidades
+                Positioned(
+                  top: 7.5 * h,
+                  left: 0.15 * w,
+                  bottom: 0.15 * w,
+                  right: 0.15 * w,
+                  child: CustomContainer(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Minhas habilidades',
+                          style: TextStyle(
+                            color: redColor,
+                            fontSize: h,
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Divider(
+                          color: redColor,
+                          thickness: 2,
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                SkillBoxes(),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )),
-        ],
-      ),
-    );
+              ],
+            )),
+          ],
+        ),
+      );
+    });
   }
 }
