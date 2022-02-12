3cdc0f34e5db2f812662a6b732f6b4c06cd3d9ad
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2022-02-12 18:28:00 -0300

    Cleaning main

--- lib/app/modules/home/models/skill_model.dart
+++
@@ -1,7 +0,0 @@
-class SkillModel {
-  String title;
-  String icon;
-  double percentage;
-
-  SkillModel({required this.title, required this.icon, required this.percentage});
-}
--- lib/app/utils/routes.dart
+++
@@ -1,9 +0,0 @@
-part of '../utils.dart';
-
-class PortfolioRoutes {
-  PortfolioRoutes._();
-
-  static String home([bool fullRoute = true]) => "/";
-  static String projects([bool fullRoute = true]) => fullRoute ? "/projects/all/" : "/all/";
-  static String info([bool fullRoute = true]) => fullRoute ? "/info/" : "/";
-}
